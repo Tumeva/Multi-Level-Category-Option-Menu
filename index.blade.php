d6ce433bd6b2bd54238d66bc9edba8d5cc6735e4
Tumeva <info@tumeva.com>
2022-10-18 09:56:20 +0300

    Update index.blade.php

--- index.blade.php
+++ index.blade.php
@@ -1,3 +1,3 @@
 <select multiple="multiple" name="catid[]" id="multiselect-optiongroup">
-{{$tmvhelper->listCategory($tmvhelper->createCategory($categories),0,$post_categories)}}
+{{$helper->listCategory($helper->createCategory($categories),0,$post_categories)}}
 </select>
